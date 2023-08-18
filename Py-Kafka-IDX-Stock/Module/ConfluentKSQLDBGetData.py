406dfa7a59c17cc9741de838830bf58abd0a7d29
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-08-18 14:35:35 +0700

    update format json

--- Py-Kafka-IDX-Stock/Module/ConfluentKSQLDBGetData.py
+++ Py-Kafka-IDX-Stock/Module/ConfluentKSQLDBGetData.py
@@ -6,19 +6,39 @@ stockQueryID = ""
 companyQueryID = ""
 
 
+async def buildJsonValue(colNames, Rows):
+    if len(colNames) == len(Rows):
+        global finalJson
+        for i in range(len(Rows)):
+            if i == 0:
+                finalJson = "{" + colNames[i] + ":" + str(Rows[i])
+            elif i == len(Rows) - 1:
+                finalJson = finalJson + "," + colNames[i] + ":" + str(Rows[i]) + "}"
+            else:
+                finalJson = finalJson + "," + colNames[i] + ":" + str(Rows[i])
+    else:
+        raise Exception("Total Column " + str(len(colNames)) + "is not match with Total Value " + str(len(Rows)))
+    return finalJson
+
+
 async def queryAsyncStock():
     global stockQueryID
     query = client.query_async("select * from ksqlgroupstock emit changes;",
                                stream_properties={"ksql.streams.auto.offset.reset": "earliest"}, timeout=None)
     counter = 0
+    global colNamesStock
     async for row in query:
         counter = counter + 1
         if counter == 1:
             stockQueryID = row['queryId']
-            # print("Query ID: ", stockQueryID)
+            colNamesStock = row['columnNames']
+            print("Stock Column Names: ", colNamesStock)
+            print("Query ID: ", stockQueryID)
         else:
-            print(
-                f"Row ID:{row[0]} StockID: {row[1]} Ticker: {row[2]} Date: {row[3]} Open: {row[4]} High: {row[5]} Low: {row[6]} Close: {row[7]} Volume: {row[8]}")
+            jsonValue = await buildJsonValue(colNamesStock, row)
+            print(jsonValue)
+            # print(f"Row ID:{row[0]} StockID: {row[1]} Ticker: {row[2]} Date: {row[3]} "
+            #       f"Open: {row[4]} High: {row[5]} Low: {row[6]} Close: {row[7]} Volume: {row[8]}")
 
 
 async def queryAsyncCompany():
@@ -26,13 +46,18 @@ async def queryAsyncCompany():
     query = client.query_async("select * from ksqlgroupcompany emit changes;",
                                stream_properties={"ksql.streams.auto.offset.reset": "earliest"}, timeout=None)
     counter = 0
+    global colNamesCompany
     async for row in query:
         counter = counter + 1
         if counter == 1:
             companyQueryID = row['queryId']
-            # print("Query ID: ", companyQueryID)
+            colNamesCompany = row['columnNames']
+            print("Company Column Names: ", colNamesCompany)
+            print("Query ID: ", companyQueryID)
         else:
-            print(f"Row ID:{row[0]} CompanyID: {row[1]} Ticker: {row[2]} Name: {row[3]} Logo: {row[4]}")
+            jsonValue = await buildJsonValue(colNamesCompany, row)
+            print(jsonValue)
+            # print(f"Row ID:{row[0]} CompanyID: {row[1]} Ticker: {row[2]} Name: {row[3]} Logo: {row[4]}")
 
 
 def shutDown():
