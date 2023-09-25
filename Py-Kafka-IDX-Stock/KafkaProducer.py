c5c01630f146b44a7394f87648f8d9ea946862a3
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-09-25 21:05:33 +0700

    update producer

--- Py-Kafka-IDX-Stock/KafkaProducer.py
+++ Py-Kafka-IDX-Stock/KafkaProducer.py
@@ -1,13 +1,14 @@
-from Module.CallAPI import getAPIResults  # Costume Module
-from Module.ClassStock import Stock, stockToDict  # Costume Module
-from Module.ClassCompany import Company, companyToDict  # Costume Module
-from confluent_kafka.schema_registry import SchemaRegistryClient  # Costume Module
-from Module.ConfluentKafkaAvroProducer import sendProducer  # Costume Module
-from Config import config, srConfig
+from Module.CallAPI import getAPIResults  # Custom Module
+from Module.ClassStock import Stock, stockToDict  # Custom Module
+from Module.ClassCompany import Company, companyToDict  # Custom Module
+from Module.ConfluentKafkaAvroProducer import sendProducer  # Custom Module
+from Config import config, srConfig  # Custom Module
 from confluent_kafka import Producer
 from confluent_kafka.serialization import StringSerializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 import datetime
+import time
 import urllib.parse
 
 if __name__ == '__main__':
@@ -33,64 +34,76 @@ if __name__ == '__main__':
     producer = Producer(config)
 
     # Query Company Trending
+    print("Start extracting trending stock")
     apiUrl = baseUrl + 'trending'
-    listTrending = getAPIResults(apiUrl)
+    responseCode, listTrending = getAPIResults(apiUrl)
+    print("End extracting trending stock")
 
     # URL List All Company
+    print("Start extracting companies")
     apiUrl2 = baseUrl + 'companies'
-    listCompany = getAPIResults(apiUrl2)
+    responseCode, listCompany = getAPIResults(apiUrl2)
+    print("End extracting companies")
 
     # Encode the parameter values
     dateTimeFormat = '%Y-%m-%d'
     currentDate = datetime.datetime.now().strftime(dateTimeFormat)
     yesterdayDate = (datetime.datetime.now() - datetime.timedelta(1)).strftime(dateTimeFormat)
     # encodedParam1 = urllib.parse.quote_plus(yesterdayDate)
-    encodedParam1 = urllib.parse.quote_plus('2020-01-02')
+    encodedParam1 = urllib.parse.quote_plus('2023-08-01')
     encodedParam2 = urllib.parse.quote_plus(currentDate)
 
-    Counter = 0
-    for row in listTrending:
-        Counter += 1
-        emitent = row['ticker']
-        change = row['change']
-        percent = row['percent']
-        print('Counter: ' + str(Counter) + ' emitent: ' + emitent + ' change: ' + change + ' percent: ' + percent)
+    try:
+        Counter = 0
+        while (True):
+            for row in listTrending:
+                Counter += 1
+                emitent = row['ticker']
+                change = row['change']
+                percent = row['percent']
+                print('Counter: ' + str(Counter) + ' emitent: ' + emitent + ' change: ' + change +
+                      ' percent: ' + percent)
 
-        # Query Historical Stock Price
-        apiUrl3 = baseUrl + emitent + '/historical'
-        apiUrl3 = apiUrl3 + '?from=' + encodedParam1 + '&to=' + encodedParam2
-        print('Counter: ' + str(Counter) + ' API Historical URL: ' + apiUrl3)
-        historicalPrices = getAPIResults(apiUrl3)
-        if len(historicalPrices) > 0 and len(listCompany) > 0:
-            for row2 in historicalPrices:
-                ticker = str(row2['ticker'])
-                date = str(row2['date'])
-                id = ticker + '_' + date
-                open = float(row2['open'])
-                high = float(row2['high'])
-                low = float(row2['low'])
-                close = float(row2['close'])
-                volume = int(row2['volume'])
-                print('Counter: ' + str(Counter) + ' ticker: ' + ticker + ' date: ' + date + ' open: ' +
-                      str(open) + ' high: ' + str(high) + ' low: ' + str(low) + ' close: ' + str(close) + ' volume: ' +
-                      str(volume))
-    
-                # Send Avro Producer
-                stock = Stock(id, ticker, date, open, high, low, close, volume)
-                sendProducer(topic1, stock, producer, stockAvroSerializer)
-    
-                filteredCompany = list(filter(lambda x: x['ticker'].lower() == emitent.lower(), listCompany))
-                # for row3 in list(filteredCompany):
-                compTicker = str(filteredCompany[0]['ticker'])
-                compName = str(filteredCompany[0]['name'])
-                compLogo = str(filteredCompany[0]['logo'])
-                print('Counter: ' + str(Counter) + ' name: ' + compName + ' logo: ' + compLogo)
-    
-                # Send Avro Producer
-                company = Company(ticker, ticker, compName, compLogo,
-                                  str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-                sendProducer(topic2, company, producer, companyAvroSerializer)
-        # Save and flush
-        producer.flush()
-        # if Counter == 1:
-        #     break
+                # Query Historical Stock Price
+                apiUrl3 = baseUrl + emitent + '/historical'
+                apiUrl3 = apiUrl3 + '?from=' + encodedParam1 + '&to=' + encodedParam2
+                responseCode, historicalPrices = getAPIResults(apiUrl3)
+                print('Counter: ' + str(Counter) + ' API Historical URL: ' + apiUrl3 +
+                      ' Response Code: ' + str(responseCode))
+                if responseCode == 200:
+                    if len(historicalPrices) > 0 and len(listCompany) > 0:
+                        for row2 in historicalPrices:
+                            ticker = str(row2['ticker'])
+                            date = str(row2['date'])
+                            id = ticker + '_' + date
+                            open = float(row2['open'])
+                            high = float(row2['high'])
+                            low = float(row2['low'])
+                            close = float(row2['close'])
+                            volume = int(row2['volume'])
+                            print('Counter: ' + str(Counter) + ' ticker: ' + ticker + ' date: ' + date + ' open: ' +
+                                  str(open) + ' high: ' + str(high) + ' low: ' + str(low) + ' close: ' + str(close) +
+                                  ' volume: ' + str(volume))
+
+                            # Send Avro Producer
+                            stock = Stock(id, ticker, date, open, high, low, close, volume)
+                            sendProducer(topic1, stock, producer, stockAvroSerializer)
+
+                            filteredCompany = list(filter(lambda x: x['ticker'].lower() == emitent.lower(), listCompany))
+                            compTicker = str(filteredCompany[0]['ticker'])
+                            compName = str(filteredCompany[0]['name'])
+                            compLogo = str(filteredCompany[0]['logo'])
+                            print('Counter: ' + str(Counter) + ' name: ' + compName + ' logo: ' + compLogo)
+
+                            # Send Avro Producer
+                            company = Company(ticker, ticker, compName, compLogo,
+                                              str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                            sendProducer(topic2, company, producer, companyAvroSerializer)
+
+                    # Save and flush
+                    producer.flush()
+
+            time.sleep(5)  # Wait 5 seconds to see the output
+    except KeyboardInterrupt:
+        print("Press Ctrl+C to terminate while statement")
+        pass
--- Py-Kafka-IDX-Stock/Module/CallAPI.py
+++ Py-Kafka-IDX-Stock/Module/CallAPI.py
@@ -6,15 +6,29 @@ def getAPIResults(apiURL):
     headers = {
         'Accept': '*/*',
         'X-API-KEY': apiKey,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+        # 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
     }
     response = requests.get(apiURL, headers=headers)
 
     if response.status_code != 200:
         print('ERROR with Response Code: ' + str(response.status_code) + ' URL: ' + apiURL)
+        return response.status_code, ""
     else:
-        return response.json()['data']['results']
+        return response.status_code, response.json()['data']['results']
 
 
 if __name__ == '__main__':
     print("THIS IS MAIN")
+
+    # baseUrl = 'https://api.goapi.id/v1/stock/idx/'
+    # apiUrl = baseUrl + 'trending'
+    # listTrending = getAPIResults(apiUrl)
+    # print(listTrending)
+    #
+    # apiUrl2 = baseUrl + 'companies'
+    # listCompany = getAPIResults(apiUrl2)
+    # print(listCompany)
+
+    # historicalURL = "https://api.goapi.id/v1/stock/idx/UNTR/historical?from=2023-08-01&to=2023-09-25"
+    # listHist = getAPIResults(historicalURL)
+    # print(listHist)
